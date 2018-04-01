3b4436f39ca8ffa95ee6cd326ce9b7a5e258f608
Adam Weil <adamjweil@gmail.com>
2018-04-01 14:09:04 -0400

    paginating

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -19,48 +19,21 @@ render() {
   return(
     <Container>
       <Row>
-        <Col md={{size: 0}} style={{backgroundColor: 'grey'}}>
-          <div className="project-navigation">
-            <Row>
-              <div className="project-nav-header">
-                <h5>
-                  <img src="https://s3.amazonaws.com/aw-personal-website-images/Project_Icon.png" style={{height: '25px', width: '25px', marginRight: '5px'}} />
-                  Projects:</h5>
-              </div>
-            </Row>
-            <ButtonGroup vertical style={{marginTop: '0px', width: '150px', marginLeft: '20px'}}>
-              <button type="button"
-                style={{borderWidth: '3px'}}
-                onClick={this.props.toggleCoinProject}
-                className={`btn btn-info + ${this.props.activeProject === 0 ? "active" : ""}`}>
-                Coin'Stache
-              </button>
-
-              <button type="button"
-                style={{borderWidth: '3px'}}
-                onClick={this.props.toggleEatProject}
-                className={`btn btn-info + ${this.props.activeProject === 1 ? "active" : ""}`}>
-                Eat & Review
-              </button>
-
-              <button type="button"
-                style={{borderWidth: '3px'}}
-                onClick={this.props.toggleInstaProject}
-                className={`btn btn-info + ${this.props.activeProject === 2 ? "active" : ""}`}>
-                React-InstaClone
-              </button>
-
-              <button type="button"
-                style={{borderWidth: '3px'}}
-                onClick={this.props.toggleScrapeProject}
-                className={`btn btn-info + ${this.props.activeProject === 3 ? "active" : ""}`}>
-                Page Scraper
-              </button>
-
-            </ButtonGroup>
-          </div>
+        <Col md={{size: 12}} style={{textAlign: 'center', marginTop: '10px'}}>
+          <h5><img src="https://s3.amazonaws.com/aw-personal-website-images/Project_Icon.png" style={{height: '25px', width: '25px', marginRight: '5px'}} />
+          Projects:
+          <ButtonGroup style={{marginLeft: "10px"}}>
+            <Button onClick={this.props.toggleCoinProject}>1</Button>
+            <Button onClick={this.props.toggleEatProject}>2</Button>
+            <Button onClick={this.props.toggleInstaProject}>3</Button>
+            <Button onClick={this.props.toggleScrapeProject}>4</Button>
+          </ButtonGroup>
+          </h5>
         </Col>
-        <Col md={{size: 10, offset: 2}} sm={{size: 12, offset: 0}}>
+      </Row>
+      <Row>
+
+        <Col md={{size: 12, offset: 0}} sm={{size: 12, offset: 0}}>
           <div className="projects">
             { this.props.activeProject === 0 ?  <CoinStache /> :
               this.props.activeProject === 1 ?  <EatReview /> :
