ce58e9fa5e516643aee6240a3aa66dee615cffeb
Adam Weil <adamweil@Adams-MBP.lan1>
2020-08-15 16:53:59 -0400

    fixing res link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -10,11 +10,11 @@ class Resume extends Component {
 render() {
   return(
           <div className="resume-container">
-              <button className="btn btn-outline-info" role="button" href="https://github.com/adamjweil/React-PersonalWebsite/files/5072580/Resume_aw.pdf" download >
+              <button className="btn btn-outline-info" role="button" href="https://github.com/adamjweil/React-PersonalWebsite/files/5079462/Resume_aw.pdf" download >
                 <span style={{marginRight: '20px'}}>Download (PDF)</span>
               </button>
               <div>
-              <img src="https://user-images.githubusercontent.com/25589910/90212208-c530f200-ddc0-11ea-8cb8-8588ddbe7b43.png" className="resumeToPrint" media="print"></img>
+              <img src="https://user-images.githubusercontent.com/25589910/90321250-fc64e780-df15-11ea-8baf-3f2781315d47.png" className="resumeToPrint" media="print"></img>
             </div>
           </div>
 
