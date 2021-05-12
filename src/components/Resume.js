334576f81342fa2c822f73282eacbf6c22e188d3
Adam Weil <adamweil@Adams-MacBook-Pro.local>
2021-05-12 16:27:34 -0400

    adding youtube link

--- src/components/Resume.js
+++ src/components/Resume.js
@@ -17,12 +17,12 @@ render() {
         <Row>
         <Col md="1"></Col>
           <Col md="10">
-            <a className="btn btn-outline-info" role="button" href="https://github.com/adamjweil/React-PersonalWebsite/files/6407182/Resume_aw.pdf" download >
+            <a className="btn btn-outline-info" role="button" href="https://github.com/adamjweil/React-PersonalWebsite/files/6468907/Resume_aw.pdf" download >
               <span style={{marginRight: '20px'}}>Download (PDF)</span>
             </a>
           </Col>
             <Row>
-                <span style={{marginLeft: '100px'}}><img src="https://user-images.githubusercontent.com/25589910/116735875-1d466d80-a9bd-11eb-9b89-37f77c1ba466.png" className="resumeToPrint" media="print"></img></span>
+                <span style={{marginLeft: '100px'}}><img src="https://user-images.githubusercontent.com/25589910/118039649-8ecbd780-b33e-11eb-928f-b4695c6de7c0.png" className="resumeToPrint" media="print"></img></span>
             </Row>
           <Col md="1"></Col>
         </Row>
